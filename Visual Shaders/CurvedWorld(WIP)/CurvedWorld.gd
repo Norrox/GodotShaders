e6eacf3e41e308e386d15cbefdcacf0642496331
Christoffer Sundbom <christoffer_karlsson@live.se>
2021-09-04 23:43:07 +0200

    Working on a world bending shader

---
+++ Visual Shaders/CurvedWorld(WIP)/CurvedWorld.gd
@@ -0,0 +1,16 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$Camera.translate(Vector3(0,0,-0.1))
